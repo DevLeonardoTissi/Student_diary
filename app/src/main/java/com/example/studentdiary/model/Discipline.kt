d010f00aa81601ef081d3ffb7c318279143ef303
leona <leonardo.tissi.si@gmail.com>
2023-03-04 13:03:03 -0300

    model Discipline.kt

---
+++ app/src/main/java/com/example/studentdiary/model/Discipline.kt
@@ -0,0 +1,9 @@
+package com.example.studentdiary.model
+
+class Discipline(
+    var name: String,
+    var description: String,
+    var startTime: String,
+    var endTime: String,
+    var favorite: Boolean
+)
