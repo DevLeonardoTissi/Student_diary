4f518f439876d89828d17582c5fa6cca1ecaae54
leona <leonardo.tissi.si@gmail.com>
2023-07-20 22:41:49 -0300

    Implementação biblioteca Browser

--- app/src/main/java/com/example/studentdiary/utils/goToUri.kt
+++ app/src/main/java/com/example/studentdiary/utils/goToUri.kt
@@ -1,11 +1,24 @@
 package com.example.studentdiary.utils
 
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import com.example.studentdiary.R
 
 fun goToUri(address: String, context: Context) {
     val uri = Uri.parse(address)
-    val intent = Intent(Intent.ACTION_VIEW, uri)
-    context.startActivity(intent)
+    val customTabsIntent = CustomTabsIntent.Builder()
+        .setDefaultColorSchemeParams(
+            CustomTabColorSchemeParams.Builder().setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary)).build()
+        )
+        .setStartAnimations(context, R.anim.enter_from_botton, R.anim.exit_to_bottom)
+        .setExitAnimations(context, R.anim.exit_to_bottom, R.anim.enter_from_botton)
+        .setUrlBarHidingEnabled(true)
+        .setShowTitle(true)
+//        .setCloseButtonIcon(toBitmap(myCustomCloseIcon))
+        .build()
+
+    customTabsIntent.launchUrl(context, uri)
 }
