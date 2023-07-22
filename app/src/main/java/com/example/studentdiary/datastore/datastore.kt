0322b510a45ea494b30b2e509cbf4448b2f28cf0
leona <leonardo.tissi.si@gmail.com>
2023-07-22 16:54:31 -0300

    asdasd

--- app/src/main/java/com/example/studentdiary/datastore/datastore.kt
+++ app/src/main/java/com/example/studentdiary/datastore/datastore.kt
@@ -5,5 +5,4 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 
-// At the top level of your kotlin file:
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "User_Token")
--- app/src/main/java/com/example/studentdiary/extensions/contextExtension.kt
+++ app/src/main/java/com/example/studentdiary/extensions/contextExtension.kt
@@ -73,11 +73,7 @@ fun Context.hideKeyboard(view: View) {
 
 
 
-fun formatTimeLeft(timeLeftInMillis: Long?):String {
-    val minutes = (timeLeftInMillis?.div(1000))?.div(60)
-    val seconds = (timeLeftInMillis?.div(1000))?.rem(60)
-    return  String.format("%02d:%02d", minutes, seconds)
 
-}
+
 
 
