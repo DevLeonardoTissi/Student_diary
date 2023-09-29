a425f2026d9611b44934762931d0aca5b91cbd34
leona <leonardo.tissi.si@gmail.com>
2023-09-29 11:43:27 -0300

    notification when sending rating

--- app/src/main/java/com/example/studentdiary/workManager/APIRatingWorker.kt
+++ app/src/main/java/com/example/studentdiary/workManager/APIRatingWorker.kt
@@ -3,6 +3,8 @@ package com.example.studentdiary.workManager
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.studentdiary.R
+import com.example.studentdiary.notifications.Notification
 import com.example.studentdiary.repository.DictionaryRepository
 import com.example.studentdiary.ui.DATA_KEY_COMMENT
 import com.example.studentdiary.ui.DATA_KEY_RATING
@@ -10,7 +12,8 @@ import com.example.studentdiary.webClient.model.RatingSender
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class APIRatingWorker(context: Context, workerParams: WorkerParameters) :CoroutineWorker(context,
+class APIRatingWorker(context: Context, workerParams: WorkerParameters) : CoroutineWorker(
+    context,
     workerParams
 ), KoinComponent {
     override suspend fun doWork(): Result {
@@ -21,8 +24,16 @@ class APIRatingWorker(context: Context, workerParams: WorkerParameters) :Corouti
 
         return try {
             repository.sendRating(RatingSender(comment = comment, rating = rating))
+
+            Notification(applicationContext)
+                .show(
+                    title = applicationContext.getString(R.string.api_rating_worker_notification_title),
+                    description = applicationContext.getString(R.string.api_rating_worker_notification_description),
+                    iconId = R.drawable.ic_info,
+                )
             Result.success()
-        }catch (e:Exception){
+
+        } catch (e: Exception) {
             Result.retry()
         }
     }
